f6754cc906c73c63dc47aeb40f8d3abcadf1524b
Namita Dongre <ndongre@andrew.cmu.edu>
2020-07-22 15:15:16 -0400

    animated skills bar onload, added project entry for moola, updated descriptions and links

--- js/skills.js
+++ js/skills.js
@@ -1,16 +1,18 @@
 // Source: https://www.darkcode.info/2020/03/animated-skills-bars-using-html-css.html
 
-$('.skill-per').each(function(){
-  var $this = $(this);
-  var per = $this.attr('per');
-  $this.css("width",per+'%');
-  $({animatedValue: 0}).animate({animatedValue: per},{
-    duration: 1000,
-    step: function(){
-      $this.attr('per', Math.floor(this.animatedValue) + '%');
-    },
-    complete: function(){
-      $this.attr('per', Math.floor(this.animatedValue) + '%');
-    }
+$(function() {
+  $('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+      duration: 1000,
+      step: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      },
+      complete: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      }
+    });
   });
 });
